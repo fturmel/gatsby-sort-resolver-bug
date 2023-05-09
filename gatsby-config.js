3597c03f2abd735dbe6b43065845ab414668221d
Francis Turmel <fturmel@gmail.com>
2023-05-09 15:47:33 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `gatsby-sort-resolver-bug`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
